34f508bffc6d6b51f66ba01e9898de00a51c5a21
tobiac <tobiac@student.ethz.ch>
2021-05-03 18:51:40 +0200

    add structure of algorithm, add core distance computation

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,11 @@
 #include <cstddef>
 #include <vector>
 
+typedef double float_t;
+
 int main() {
 
-    double *dataset = NULL;
+    float_t *dataset = NULL;
     int *labels = NULL;
     int shape[2];
     const char *filename = "../data/blobs_0.csv";
@@ -36,18 +38,29 @@ int main() {
 	}
 	printf("...\n\n");
 
-    free(dataset);
-    free(labels);
+    // 1. Compute core distances
+    int mpts = 4;
+    float_t *core_dist = static_cast<float_t *>(malloc(n * sizeof(float_t)));
+    compute_core_distances(dataset, core_dist, mpts, n, d);
+
+    printf("Core distances computed.\n");
+
+    // 2. Compute the MST -> mutual reachability graph
+    //prim_advanced(dataset, core_dist, );
+
+    // 3. Extend graph
+
+    // 4. Build hierarchical tree
 
 	int* edgesA;
 	int* edgesB;
 	float_t* distances;
 	int minimum_cluster_size = 10;
-	
+
 	std::vector<Cluster> condensed_cluster_tree = clustering(edgesA,edgesB,distances,n,minimum_cluster_size);
-	
+
 	std::vector<Cluster> selected_clusters = extract_clusters(condensed_cluster_tree);
-	
+
 	int* our_labels = point_labels(selected_clusters,n);
 
     return 0;
