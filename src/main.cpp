fefdb2f4f29d1f33a666fb36974f1911388cfb08
tobiac <tobiac@student.ethz.ch>
2021-05-03 21:34:32 +0200

    Finish Algorithm, but fails condesing the tree

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "utils.h"
 #include "clustering.h"
 #include "cluster.h"
+#include "prim.h"
 
 #include <cstdlib>
 #include <cstdio>
@@ -39,29 +40,58 @@ int main() {
 	printf("...\n\n");
 
     // 1. Compute core distances
-    int mpts = 4;
-    float_t *core_dist = static_cast<float_t *>(malloc(n * sizeof(float_t)));
-    compute_core_distances(dataset, core_dist, mpts, n, d);
+    int mpts = 3;
+    float_t *core_dist = static_cast<float_t *>(malloc(n * n * sizeof(float_t)));
+    compute_distance_matrix(dataset, core_dist, mpts, n, d);
 
     printf("Core distances computed.\n");
 
     // 2. Compute the MST -> mutual reachability graph
-    //prim_advanced(dataset, core_dist, );
+    int n_ext = 2*n - 1;
+    edge *mst = static_cast<edge *>(malloc(n_ext * sizeof(edge)));
+    prim(core_dist, mst, n);
 
-    // 3. Extend graph
+    printf("MST computed. First edge in arr : (%i, %i) | weight : %f\n", mst[0].u, mst[0].v, mst[0].weight);
+
+    // 3. Extend graph with self-edge -> MST_{ext}
+    for(int i = 0; i < n; ++i){
+        mst[(n-1) + i] = {core_dist[i*n + i], i, i};
+    }
+
+    printf("MST extended. Last self-edge in arr : (%i, %i) | weight : %f\n", mst[n_ext-1].u, mst[n_ext-1].v, mst[n_ext-1].weight);
 
     // 4. Build hierarchical tree
 
-	int* edgesA;
-	int* edgesB;
-	float_t* distances;
+    // 4.1 Convert AoS mst into SoA
+    // (optimization easily done with SIMD instructions, or rather unify the interface :D)
+	int* edges_A;
+	int* edges_B;
+	float_t* core_dist_ext;
+
+    edges_A = static_cast<int *>(malloc(n_ext * sizeof(int)));
+    edges_B = static_cast<int *>(malloc(n_ext * sizeof(int)));
+    core_dist_ext = static_cast<float_t *>(malloc(n_ext * sizeof(float_t)));
+
+    // Do conversion (quick and dirty)
+    for(int i = 0; i < n_ext; ++i){
+        edges_A[i] = mst[i].u;
+        edges_B[i] = mst[i].v;
+        core_dist_ext[i] = mst[i].weight;
+    }
+
+    // 4.2 Build the hierarchical tree itself
+
 	int minimum_cluster_size = 10;
 
-	std::vector<Cluster> condensed_cluster_tree = clustering(edgesA,edgesB,distances,n,minimum_cluster_size);
+	std::vector<Cluster> condensed_cluster_tree = clustering(edges_A, edges_B, core_dist_ext, n_ext, minimum_cluster_size);
+
+    printf("Finished creating condensed cluster tree");
 
 	std::vector<Cluster> selected_clusters = extract_clusters(condensed_cluster_tree);
 
-	int* our_labels = point_labels(selected_clusters,n);
+    printf("Finished extracting clusters. Found %i clusters. \n", selected_clusters.size());
+
+	int* our_labels = point_labels(selected_clusters, n_ext);
 
     return 0;
 
