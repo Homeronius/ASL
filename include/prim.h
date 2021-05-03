10da1a4afb238fe29f75e05d5f1bfb37e5c9f379
Alexander Haegele <alexanderhaegele@t-online.de>
2021-05-03 16:35:29 +0200

    introduce edge struct for result array of mst

--- include/prim.h
+++ include/prim.h
@@ -1,5 +1,14 @@
 #ifndef _HDBSCAN_PRIM_H_
 #define _HDBSCAN_PRIM_H_
-void prim(double *adjacency, int *result, int n);
-void prim_advanced(double *X, double *core_distances, int *result, int n, int d);
+
+struct edge {
+    double weight;
+    int u;
+    int v;
+};
+
+typedef struct edge edge;
+
+void prim(double *adjacency, edge *result, int n);
+void prim_advanced(double *X, double *core_distances, edge *result, int n, int d);
 #endif  // _HDBSCAN_PRIM_H
--- src/prim.cpp
+++ src/prim.cpp
@@ -11,11 +11,11 @@
  *        edge weights. The graph is assumed to be undirected.
  * 
  * @param adjacency the adjacency matrix of the graph [n,n]
- * @param result pointer to store result into @TODO: figure out the result format
+ * @param result pointer to store result into
  * @param n number of nodes in the graph
  * 
  */
-void prim(double *adjacency, int *result, int n) {
+void prim(double *adjacency, edge *result, int n) {
     // represent tree via each node's parent?
     int parent[n];
     // cheapest cost of edge to node
@@ -23,16 +23,19 @@ void prim(double *adjacency, int *result, int n) {
     // nodes contained in tree
     bool contained[n];
 
-    for (int i = 0; i < n; i++) {
-        cost[i] = __DBL_MAX__;
-        contained[i] = false;
-    }
-
     // start from vertex 0
     cost[0] = 0.0;
     parent[0] = -1;
+    contained[0] = true;
 
-    for (int iter = 0; iter < n; iter++) {
+    // all other costs initialized to dist to 0
+    for (int i = 1; i < n; i++) {
+        cost[i] = adjacency[i];
+        contained[i] = false;
+        parent[i] = 0;
+    }
+
+    for (int iter = 0; iter < n - 1; iter++) {
         // i is the node we want to add
         int i, j;
 
@@ -45,7 +48,7 @@ void prim(double *adjacency, int *result, int n) {
 
         // add i to MST
         contained[i] = true;
-
+        result[iter] = {min, parent[i], i};
         // update the cost values of the nodes that
         // are not yet in MST with possible lower distance to i
         for (j = 0; j < n; j++)
@@ -61,12 +64,12 @@ void prim(double *adjacency, int *result, int n) {
  * 
  * @param X the input data of shape [n,d]
  * @param core_distances array of core_distance for each sample [n]
- * @param result pointer to store result into @TODO: figure out the result format
+ * @param result pointer to store result into
  * @param n number of nodes in the graph
  * @param d the dimensionality of the data
  * 
  */
-void prim_advanced(double *X, double *core_distances, int *result, int n, int d) {
+void prim_advanced(double *X, double *core_distances, edge *result, int n, int d) {
     // represent tree via each node's parent?
     int parent[n];
     // current minimal distance found to node
@@ -88,6 +91,7 @@ void prim_advanced(double *X, double *core_distances, int *result, int n, int d)
             curr_min_cost_j, dist_between, mutual_reach_dist;
 
         // add i to MST
+        // @TODO: properly add result here
         contained[i] = true;
 
         core_dist_i = core_distances[i];
@@ -102,7 +106,7 @@ void prim_advanced(double *X, double *core_distances, int *result, int n, int d)
 
             curr_min_cost_j = cost[j];
             core_dist_j = core_distances[j];
-            // @TODO have distance function here
+            // @TODO different distance funcs?
             dist_between = euclidean_distance(&X[i * n], &X[j * n], d);
 
             mutual_reach_dist = fmax(core_dist_i, fmax(core_dist_j, dist_between));
