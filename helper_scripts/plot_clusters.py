3102f7fff72361c472d25f2d609614275f89c401
tobiac <tobiac@student.ethz.ch>
2021-05-04 21:31:56 +0200

    add colorblind mode from seaborn

--- helper_scripts/plot_clusters.py
+++ helper_scripts/plot_clusters.py
@@ -1,11 +1,12 @@
 import sys
 import numpy
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 def read_dataset(path):
     data = numpy.loadtxt(open(path, "rb"), delimiter=",", skiprows=1)
     return data[:,:-1], data[:,-1]
-            
+
 
 def plot_dataset2D(X, y):
     """Plot first two features of dataset.
@@ -17,20 +18,22 @@ def plot_dataset2D(X, y):
     y :
         labels (n)
     """
+    sns.set_theme()
+    sns.color_palette('colorblind')
 
     fig = plt.figure()
-    
+
     ax = fig.add_subplot(111)
 
     ax.scatter(X[:, 0], X[:, 1], marker="o", c=y, s=20, edgecolor="k")
     # for i in range(X.shape[0]):
     #     ax.annotate(i, (X[i, 0]+.1, X[i, 1]+.1))
-    
+
     # plt.axis('equal')
     plt.axis('square')
 
     plt.show()
-    
+
 
 
 def main():
