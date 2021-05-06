cf3302664003f37c243ccb8841e1e515d02ff227
tobiac <tobiac@student.ethz.ch>
2021-05-06 18:05:58 +0200

    script to run benchmark binary on multiple datasets and print output to file

---
+++ run_perf_measurements.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Run the benchmark binary on a number of '.csv' datasets and store output to a file
+
+BINARY="./build/bin/hdbscan_benchmark"
+OUTPUT="timings.out"
+# Substring input the datasets have in common
+FILEBASE="data/perf_data"
+# Number of datasets to be processed
+N=3
+
+if [ -s $OUTPUT ]
+then
+    printf "File already exists. Appending output ...\n"
+    printf "======================\
+            \nOutput from new performance run:\
+            \n======================\n" >> $OUTPUT
+else
+    touch $OUTPUT
+fi
+
+for i in $(seq 0 $N);
+do
+    printf "======================\
+            \nMeasurement for ${FILEBASE}_${i}.csv\
+            \n======================\n" >> $OUTPUT
+    ${BINARY} "${FILEBASE}_${i}.csv" >> $OUTPUT
+    printf "\n" >> $OUTPUT
+done
