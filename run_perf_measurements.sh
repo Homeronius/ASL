36437668d5124bc7be5a7a2e63388e5759520423
Alexander Haegele <alexanderhaegele@t-online.de>
2021-05-07 14:01:59 +0200

    include csv file storage in run_perf_measurements.sh

--- run_perf_measurements.sh
+++ run_perf_measurements.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
-# Run the benchmark binary on a number of '.csv' datasets and store output to a file
+# Run the benchmark binary on a number of '.csv' datasets and store the
+# output to a log as well as .csv file
 
 BINARY="./build/bin/hdbscan_benchmark"
-OUTPUT="timings.out"
+OUTPUT_LOG="timings.out"
+OUTPUT_CSV="data/timings.csv"
 # Substring input the datasets have in common
 FILEBASE="data/perf_data"
 # Number of datasets to be processed
 N=9
 
-if [ -s ${OUTPUT} ]
+if [ -s ${OUTPUT_LOG} ]
 then
-    printf "File already exists. Appending output ...\n"
+    printf "File already exists. Appending OUTPUT_LOG ...\n"
     printf "======================\
-            \nOutput from new performance run:\
-            \n======================\n" >> ${OUTPUT}
+            \nOUTPUT_LOG from new performance run:\
+            \n======================\n" >> ${OUTPUT_LOG}
 else
-    touch ${OUTPUT}
+    touch ${OUTPUT_LOG}
 fi
 
 for i in $(seq 0 ${N});
 do
     printf "======================\
             \nMeasurement for ${FILEBASE}_${i}.csv\
-            \n======================\n" >> ${OUTPUT}
-    ${BINARY} "${FILEBASE}_${i}.csv" >> ${OUTPUT}
-    printf "\n" >> ${OUTPUT}
+            \n======================\n" >> ${OUTPUT_LOG}
+    ${BINARY} "${FILEBASE}_${i}.csv" "${OUTPUT_CSV}" >> ${OUTPUT_LOG}
+    printf "\n" >> ${OUTPUT_LOG}
 done
