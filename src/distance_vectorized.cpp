7ecfc33dc60fb9b6521478c8ea83e82c226654f1
Martin Erhart <maerhart@outlook.com>
2021-05-11 17:36:46 +0200

    Working partition vectorization

--- src/distance_vectorized.cpp
+++ src/distance_vectorized.cpp
@@ -32,7 +32,7 @@ void euclidean_distance_2(double *pa1, double *pa2, double *pb1, double *pb2,
   // sqrt = | sqrt(a) | sqrt(b) | sqrt(c) | sqrt(d) |
   __m256d sqrt = _mm256_sqrt_pd(add1);
 
-  _mm256_store_pd(res, sqrt);
+  _mm256_storeu_pd(res, sqrt);
 
 #ifdef HDBSCAN_INSTRUMENT
   hdbscan_sqrt_counter += 4;
@@ -71,7 +71,7 @@ void euclidean_distance_4(double *pa1, double *pa2, double *pb1, double *pb2,
   __m256d add3 = _mm256_hadd_pd(perm1, perm2);
 
   __m256d sqrt = _mm256_sqrt_pd(add3);
-  _mm256_store_pd(res, sqrt);
+  _mm256_storeu_pd(res, sqrt);
 
 #ifdef HDBSCAN_INSTRUMENT
   hdbscan_sqrt_counter += 4;
@@ -114,7 +114,7 @@ void euclidean_distance_4_opt(double *base, double *p1, double *p2, double *p3,
   __m256d sum = _mm256_hadd_pd(perm1, perm2);
 
   __m256d sqrt = _mm256_sqrt_pd(sum);
-  _mm256_store_pd(res, sqrt);
+  _mm256_storeu_pd(res, sqrt);
 
 #ifdef HDBSCAN_INSTRUMENT
   hdbscan_sqrt_counter += 4;
@@ -133,7 +133,7 @@ double euclidean_distance(double *p1, double *p2, int d) {
   }
 
   double buff[4];
-  _mm256_store_pd(buff, sum);
+  _mm256_storeu_pd(buff, sum);
 
   double res = buff[0] + buff[1] + buff[2] + buff[3];
 
@@ -198,7 +198,7 @@ void manhattan_distance_2(double *pa1, double *pa2, double *pb1, double *pb2,
 
   __m256d add = _mm256_hadd_pd(sum_ab, sum_cd);
 
-  _mm256_store_pd(res, add);
+  _mm256_storeu_pd(res, add);
 }
 
 void manhattan_distance_4(double *pa1, double *pa2, double *pb1, double *pb2,
@@ -233,7 +233,7 @@ void manhattan_distance_4(double *pa1, double *pa2, double *pb1, double *pb2,
   __m256d perm2 = _mm256_permute4x64_pd(add2, 0b11011000);
 
   __m256d add3 = _mm256_hadd_pd(perm1, perm2);
-  _mm256_store_pd(res, add3);
+  _mm256_storeu_pd(res, add3);
 }
 
 inline void swap(double *a, double *b) {
@@ -251,6 +251,7 @@ void printlist(double *list, int left, int right) {
       printf(", ");
     }
     printf("%f", list[i]);
+    first = false;
   }
   printf("]\n");
 }
@@ -350,6 +351,97 @@ int partition_vec(double *list, int left, int right, int pivot) {
   return store_idx;
 }
 
+void partition_vec2(double *list, double *lower, int *idx_lower,
+                    int *idx_higher, int n, int pivot) {
+  double pivot_val = list[pivot];
+  int store_idx_lower = 0;
+  int store_idx_higher = 0;
+  int i = 0;
+
+  swap(list + pivot, list + n - 1);
+
+  __m256d pivotvec = _mm256_set1_pd(pivot_val);
+
+  for (; i < n - 4; i += 4) {
+    __m256d listvec = _mm256_loadu_pd(list + i);
+    {
+      __m256d less_mask = _mm256_cmp_pd(listvec, pivotvec, _CMP_LE_OQ);
+      int mask = _mm256_movemask_pd(less_mask);
+      int count = __builtin_popcount(mask);
+
+      // Unpack each bit to a byte
+      unsigned int expanded_mask = _pdep_u32(mask, 0x01010101);
+      // Replicate bit to fill entire byte
+      expanded_mask *= 0xFF;
+
+      const unsigned int identity_indices = 0x76543210;
+      unsigned int wanted_indices = _pext_u32(identity_indices, expanded_mask);
+
+      // TODO: is there a more elegant way to do this?
+      __m256i shufmask = _mm256_set_epi32(
+          (wanted_indices >> 28) & 0xF, (wanted_indices >> 24) & 0xF,
+          (wanted_indices >> 20) & 0xF, (wanted_indices >> 16) & 0xF,
+          (wanted_indices >> 12) & 0xF, (wanted_indices >> 8) & 0xF,
+          (wanted_indices >> 4) & 0xF, wanted_indices & 0xF);
+
+      // Unfortunately, there is no intrinsic in AVX2 to do double permutevar
+      __m256d res = _mm256_castps_pd(
+          _mm256_permutevar8x32_ps(_mm256_castpd_ps(listvec), shufmask));
+
+      __m256i sm = _mm256_castpd_si256(_mm256_cmp_pd(
+          _mm256_castsi256_pd(shufmask), _mm256_set1_pd(0), _CMP_NEQ_OQ));
+
+      _mm256_maskstore_pd(lower + store_idx_lower, sm, res);
+
+      store_idx_lower += count;
+    }
+    {
+      __m256d less_mask = _mm256_cmp_pd(listvec, pivotvec, _CMP_GT_OQ);
+      int mask = _mm256_movemask_pd(less_mask);
+      int count = __builtin_popcount(mask);
+
+      // Unpack each bit to a byte
+      unsigned int expanded_mask = _pdep_u32(mask, 0x01010101);
+      // Replicate bit to fill entire byte
+      expanded_mask *= 0xFF;
+
+      const unsigned int identity_indices = 0x76543210;
+      unsigned int wanted_indices = _pext_u32(identity_indices, expanded_mask);
+
+      // TODO: is there a more elegant way to do this?
+      __m256i shufmask = _mm256_set_epi32(
+          (wanted_indices >> 28) & 0xF, (wanted_indices >> 24) & 0xF,
+          (wanted_indices >> 20) & 0xF, (wanted_indices >> 16) & 0xF,
+          (wanted_indices >> 12) & 0xF, (wanted_indices >> 8) & 0xF,
+          (wanted_indices >> 4) & 0xF, wanted_indices & 0xF);
+
+      // Unfortunately, there is no intrinsic in AVX2 to do double permutevar
+      __m256d res = _mm256_castps_pd(
+          _mm256_permutevar8x32_ps(_mm256_castpd_ps(listvec), shufmask));
+
+      __m256i sm = _mm256_castpd_si256(_mm256_cmp_pd(
+          _mm256_castsi256_pd(shufmask), _mm256_set1_pd(0), _CMP_NEQ_OQ));
+
+      _mm256_maskstore_pd(list + store_idx_higher, sm, res);
+      store_idx_higher += count;
+    }
+  }
+
+  for (; i < n - 1; i++) {
+    if (list[i] <= pivot_val) {
+      lower[store_idx_lower] = list[i];
+      store_idx_lower++;
+    }
+    if (list[i] > pivot_val) {
+      list[store_idx_higher] = list[i];
+      store_idx_higher++;
+    }
+  }
+
+  *idx_higher = store_idx_higher;
+  *idx_lower = store_idx_lower;
+}
+
 double quickselect(double *list, int left, int right, int k) {
   if (left == right) {
     return list[left];
@@ -398,6 +490,42 @@ double iterative_quickselect(double *list, int left, int right, int k) {
   }
 }
 
+double iterative_quickselect2(double *list, int n, int k) {
+  double lo[n];
+  double *lower = lo;
+  int pivot_idx = n / 2;
+
+  if (k >= n) {
+    return NAN;
+  }
+
+  while (true) {
+    if (n == 1) {
+      return list[0];
+    }
+
+    // Take element in the middle as pivot
+    double pivot_val = list[pivot_idx];
+    int store_idx_lower;
+    int store_idx_higher;
+    partition_vec2(list, lower, &store_idx_lower, &store_idx_higher, n,
+                   pivot_idx);
+
+    if (store_idx_lower == k) {
+      return pivot_val;
+    } else if (store_idx_lower > k) {
+      double *tmp = list;
+      list = lower;
+      lower = tmp;
+      n = store_idx_lower;
+    } else {
+      k -= store_idx_lower + 1;
+      n = store_idx_higher;
+    }
+    pivot_idx = n / 2;
+  }
+}
+
 void compute_core_distances(double *input, double *core_dist, int mpts, int n,
                             int d) {
   double distances[n];
@@ -412,7 +540,8 @@ void compute_core_distances(double *input, double *core_dist, int mpts, int n,
       //                      input + (i + 3) * d, distances + i);
     }
 
-    core_dist[k] = iterative_quickselect(distances, 0, n - 1, mpts - 1);
+    // core_dist[k] = iterative_quickselect(distances, 0, n - 1, mpts - 1);
+    core_dist[k] = iterative_quickselect2(distances, n, mpts - 1);
   }
 }
 
