cc244b3485fb408bc7e2462805c2abf4f729c54d
Alexander Haegele <alexanderhaegele@t-online.de>
2021-05-14 12:11:03 +0200

    uhave perf script use specific benchm. name and write to logs/ and data/timings/

--- run_perf_measurements.sh
+++ run_perf_measurements.sh
@@ -2,30 +2,37 @@
 
 # Run the benchmark binary on a number of '.csv' datasets and store the
 # output to a log as well as .csv file
+if [ $# -eq 0 ]; then
+    echo "Usage: $(basename $0) <benchm_name>"
+    exit 1
+fi
 
+# make log directories if not existing
+mkdir -p logs
+mkdir -p data/timings
+
+SUFFIX=$1
 BINARY="./build/bin/hdbscan_benchmark"
-OUTPUT_LOG="timings.out"
-OUTPUT_CSV="data/timings.csv"
+OUTPUT_LOG="logs/timings_${SUFFIX}.out"
+OUTPUT_CSV="data/timings/timings_${SUFFIX}.csv"
 # Substring input the datasets have in common
 FILEBASE="data/perf_data"
 # Number of datasets to be processed
 N=9
 
-if [ -s ${OUTPUT_LOG} ]
-then
+if [ -s ${OUTPUT_LOG} ]; then
     printf "File already exists. Appending OUTPUT_LOG ...\n"
     printf "======================\
             \nOUTPUT_LOG from new performance run:\
-            \n======================\n" >> ${OUTPUT_LOG}
+            \n======================\n" >>${OUTPUT_LOG}
 else
     touch ${OUTPUT_LOG}
 fi
 
-for i in $(seq 0 ${N});
-do
+for i in $(seq 0 ${N}); do
     printf "======================\
             \nMeasurement for ${FILEBASE}_${i}.csv\
-            \n======================\n" >> ${OUTPUT_LOG}
-    ${BINARY} "${FILEBASE}_${i}.csv" "${OUTPUT_CSV}" >> ${OUTPUT_LOG}
-    printf "\n" >> ${OUTPUT_LOG}
+            \n======================\n" >>${OUTPUT_LOG}
+    ${BINARY} "${FILEBASE}_${i}.csv" "${OUTPUT_CSV}" >>${OUTPUT_LOG}
+    printf "\n" >>${OUTPUT_LOG}
 done
