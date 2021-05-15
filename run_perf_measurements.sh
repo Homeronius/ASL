ddc47a586d716a7492f310b12e42c0bff8189d85
Alexander Haegele <alexanderhaegele@t-online.de>
2021-05-15 18:13:42 +0200

    run perf measurements with more arguments; prints time to log

--- run_perf_measurements.sh
+++ run_perf_measurements.sh
@@ -2,8 +2,8 @@
 
 # Run the benchmark binary on a number of '.csv' datasets and store the
 # output to a log as well as .csv file
-if [ $# -eq 0 ]; then
-    echo "Usage: $(basename $0) <benchm_name>"
+if [ $# -ne 4 ]; then
+    echo "Usage: $(basename $0) <benchm_name> <binary_name> <filebase> <n_datasets>"
     exit 1
 fi
 
@@ -12,13 +12,13 @@ mkdir -p logs
 mkdir -p data/timings
 
 SUFFIX=$1
-BINARY="./build/bin/hdbscan_benchmark"
+BINARY="./build/bin/$2"
 OUTPUT_LOG="logs/timings_${SUFFIX}.out"
 OUTPUT_CSV="data/timings/${SUFFIX}.csv"
 # Substring input the datasets have in common
-FILEBASE="data/perf_data"
+FILEBASE="data/$3"
 # Number of datasets to be processed
-N=9
+N=$4
 
 if [ -s ${OUTPUT_LOG} ]; then
     printf "File already exists. Appending OUTPUT_LOG ...\n"
@@ -28,9 +28,9 @@ if [ -s ${OUTPUT_LOG} ]; then
 else
     touch ${OUTPUT_LOG}
 fi
-
+printf "Running test at time %s\n" "$(date)" >>${OUTPUT_LOG}
 for i in $(seq 0 ${N}); do
-    echo "running benchmark for ${FILEBASE}_${i}.csv ..."
+    echo "running benchmark \"${SUFFIX}\" for ${FILEBASE}_${i}.csv ..."
     printf "======================\
             \nMeasurement for ${FILEBASE}_${i}.csv\
             \n======================\n" >>${OUTPUT_LOG}
