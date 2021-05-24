efb6f0b876303626757a568618d8bcf1733c6e80
tobiac <tobiac@student.ethz.ch>
2021-05-24 20:40:52 +0200

    add function declarations to use them in prim_vectorized.cpp

--- include/distance.h
+++ include/distance.h
@@ -8,7 +8,13 @@ void compute_distance_matrix(double *input, double *core_dist, double *dist,
 void compute_distance_matrix_triang(double *input, double *core_dist,
                                     double *dist, int mpts, int n, int d);
 void compute_distance_matrix_blocked(double *input, double *core_dist,
-                                      double *dist, int mpts, int n, int d);
+                                     double *dist, int mpts, int n, int d);
+
+void euclidean_distance_4_opt(double *base, double *p1, double *p2, double *p3,
+                              double *p4, double *res);
+void euclidean_distance_2(double *pa1, double *pa2, double *pb1, double *pb2,
+                          double *pc1, double *pc2, double *pd1, double *pd2,
+                          double *res);
 double euclidean_distance(double *p1, double *p2, int d);
 double manhattan_distance(double *p1, double *p2, int d);
 double quickselect(double *list, int left, int right, int k);
