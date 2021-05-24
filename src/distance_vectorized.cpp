6508c55f9ebfc221926314112d6a104f5dfe52b5
Martin Erhart <maerhart@outlook.com>
2021-05-24 16:26:46 +0200

    Use unaligned load intrinsics

--- src/distance_vectorized.cpp
+++ src/distance_vectorized.cpp
@@ -42,23 +42,23 @@ void euclidean_distance_2(double *pa1, double *pa2, double *pb1, double *pb2,
 void euclidean_distance_4(double *pa1, double *pa2, double *pb1, double *pb2,
                           double *pc1, double *pc2, double *pd1, double *pd2,
                           double *res) {
-  __m256d pva1 = _mm256_load_pd(pa1);
-  __m256d pva2 = _mm256_load_pd(pa2);
+  __m256d pva1 = _mm256_loadu_pd(pa1);
+  __m256d pva2 = _mm256_loadu_pd(pa2);
   __m256d diffa = _mm256_sub_pd(pva1, pva2);
   __m256d suma = _mm256_mul_pd(diffa, diffa);
 
-  __m256d pvb1 = _mm256_load_pd(pb1);
-  __m256d pvb2 = _mm256_load_pd(pb2);
+  __m256d pvb1 = _mm256_loadu_pd(pb1);
+  __m256d pvb2 = _mm256_loadu_pd(pb2);
   __m256d diffb = _mm256_sub_pd(pvb1, pvb2);
   __m256d sumb = _mm256_mul_pd(diffb, diffb);
 
-  __m256d pvc1 = _mm256_load_pd(pc1);
-  __m256d pvc2 = _mm256_load_pd(pc2);
+  __m256d pvc1 = _mm256_loadu_pd(pc1);
+  __m256d pvc2 = _mm256_loadu_pd(pc2);
   __m256d diffc = _mm256_sub_pd(pvc1, pvc2);
   __m256d sumc = _mm256_mul_pd(diffc, diffc);
 
-  __m256d pvd1 = _mm256_load_pd(pd1);
-  __m256d pvd2 = _mm256_load_pd(pd2);
+  __m256d pvd1 = _mm256_loadu_pd(pd1);
+  __m256d pvd2 = _mm256_loadu_pd(pd2);
   __m256d diffd = _mm256_sub_pd(pvd1, pvd2);
   __m256d sumd = _mm256_mul_pd(diffd, diffd);
 
@@ -80,28 +80,28 @@ void euclidean_distance_4(double *pa1, double *pa2, double *pb1, double *pb2,
 
 void euclidean_distance_4_opt(double *base, double *p1, double *p2, double *p3,
                               double *p4, double *res) {
-  __m256d base_vec = _mm256_load_pd(base);
+  __m256d base_vec = _mm256_loadu_pd(base);
 
   __m256d partial_sum1;
   __m256d partial_sum2;
 
   {
-    __m256d p1_vec = _mm256_load_pd(p1);
+    __m256d p1_vec = _mm256_loadu_pd(p1);
     __m256d diff1 = _mm256_sub_pd(base_vec, p1_vec);
     __m256d sum1 = _mm256_mul_pd(diff1, diff1);
 
-    __m256d pvb2 = _mm256_load_pd(p2);
+    __m256d pvb2 = _mm256_loadu_pd(p2);
     __m256d diff2 = _mm256_sub_pd(base_vec, pvb2);
     __m256d sum2 = _mm256_mul_pd(diff2, diff2);
 
     partial_sum1 = _mm256_hadd_pd(sum1, sum2);
   }
   {
-    __m256d p3_vec = _mm256_load_pd(p3);
+    __m256d p3_vec = _mm256_loadu_pd(p3);
     __m256d diff3 = _mm256_sub_pd(base_vec, p3_vec);
     __m256d sum3 = _mm256_mul_pd(diff3, diff3);
 
-    __m256d p4_vec = _mm256_load_pd(p4);
+    __m256d p4_vec = _mm256_loadu_pd(p4);
     __m256d diff4 = _mm256_sub_pd(base_vec, p4_vec);
     __m256d sum4 = _mm256_mul_pd(diff4, diff4);
 
@@ -207,23 +207,23 @@ void manhattan_distance_4(double *pa1, double *pa2, double *pb1, double *pb2,
                           double *res) {
   __m256d abs_mask =
       _mm256_castsi256_pd(_mm256_set1_epi64x(0x7FFFFFFFFFFFFFFF));
-  __m256d pa1_vec = _mm256_load_pd(pa1);
-  __m256d pa2_vec = _mm256_load_pd(pa2);
+  __m256d pa1_vec = _mm256_loadu_pd(pa1);
+  __m256d pa2_vec = _mm256_loadu_pd(pa2);
   __m256d diffa = _mm256_sub_pd(pa1_vec, pa2_vec);
   __m256d absa = _mm256_and_pd(diffa, abs_mask);
 
-  __m256d pb1_vec = _mm256_load_pd(pb1);
-  __m256d pb2_vec = _mm256_load_pd(pb2);
+  __m256d pb1_vec = _mm256_loadu_pd(pb1);
+  __m256d pb2_vec = _mm256_loadu_pd(pb2);
   __m256d diffb = _mm256_sub_pd(pb1_vec, pb2_vec);
   __m256d absb = _mm256_and_pd(diffb, abs_mask);
 
-  __m256d pc1_vec = _mm256_load_pd(pc1);
-  __m256d pc2_vec = _mm256_load_pd(pc2);
+  __m256d pc1_vec = _mm256_loadu_pd(pc1);
+  __m256d pc2_vec = _mm256_loadu_pd(pc2);
   __m256d diffc = _mm256_sub_pd(pc1_vec, pc2_vec);
   __m256d absc = _mm256_and_pd(diffc, abs_mask);
 
-  __m256d pd1_vec = _mm256_load_pd(pd1);
-  __m256d pd2_vec = _mm256_load_pd(pd2);
+  __m256d pd1_vec = _mm256_loadu_pd(pd1);
+  __m256d pd2_vec = _mm256_loadu_pd(pd2);
   __m256d diffd = _mm256_sub_pd(pd1_vec, pd2_vec);
   __m256d absd = _mm256_and_pd(diffd, abs_mask);
 
