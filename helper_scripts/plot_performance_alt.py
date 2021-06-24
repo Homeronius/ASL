3e8fd7ef07b119e31b1d4cff51f3dc7d5ba927f6
tobiac <tobiac@student.ethz.ch>
2021-06-24 14:10:02 +0200

    add power of 2 ticks in linear

--- helper_scripts/plot_performance_alt.py
+++ helper_scripts/plot_performance_alt.py
@@ -4,17 +4,23 @@ from typing import Type
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from math import log2
 
 AMD = r" $\bf{AMD\ Ryzen\ 7\ 4800H\ (Zen\ 2),\ 2.9GHz}$"
 INTEL = r" $\bf{Intel\ i5-7300U\ (Kaby\ Lake),\ 2.6GHz}$"
 
 
+def format_func(value, tick_number):
+    # find exponent for tick
+    return r"$2^{{ {:} }}$".format(int(log2(value)))
+
+
 def read_dataset(path, system="intel"):
     data = np.loadtxt(open(path, "rb"), delimiter=",", skiprows=1)
     if system == "intel":
-        return data[:, 0], data[:, 1], data[:, 6], data[:, 2], data[:, 3],data[:,10]
+        return data[:, 0], data[:, 1], data[:, 6], data[:, 2], data[:, 3], data[:, 10]
     else:
-        return data[:, 0], data[:, 1], data[:, 6], data[:, 2], data[:, 3],data[:,11]
+        return data[:, 0], data[:, 1], data[:, 6], data[:, 2], data[:, 3], data[:, 11]
 
 
 def main(args):
@@ -61,8 +67,8 @@ def main(args):
             raise TypeError("unsupported metric to plot")
 
         if not mpts_mode and not dim_mode:
-          N = N[2:]
-          y = y[2:]
+            N = N[2:]
+            y = y[2:]
 
         X_axis = []
         if mpts_mode:
@@ -75,6 +81,8 @@ def main(args):
         if args.x_scale == "linear":
             (line,) = ax.plot(X_axis, y, linestyle="-", marker="o")
             ax.xaxis.set_major_locator(plt.MultipleLocator(2048))
+            # ax.xaxis.set_major_locator(plt.LogLocator(base=2, subs=[16]))
+            # ax.xaxis.set_major_formatter(plt.FuncFormatter(format_func))
         elif args.x_scale == "log":
             (line,) = ax.semilogx(X_axis, y, linestyle="-", marker="o", base=2)
         else:
@@ -84,7 +92,7 @@ def main(args):
 
     ax.set_xlabel("k" if mpts_mode else ("d" if dim_mode else "n"))
     # ax.set_ylabel('flops/cycle')
-    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    ax.legend(loc="center left", bbox_to_anchor=(1, 0.5))
 
     if args.save_path is None:
         plt.show()
